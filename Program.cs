7874fca8fe69033f006b48f35e51e9e34598258a
Amr Fouad <68440701+3mrfouad@users.noreply.github.com>
2020-08-15 02:48:00 -0600

    Dataset display, menu options, and program exit sequance completed

--- Program.cs
+++ Program.cs
@@ -7,67 +7,107 @@ namespace c_assignment_crud_3mrfouad
     {
         static void Main(string[] args)
         {
-            // Req#1: Allow the user to input data of your choice (names, numbers, etc).
-            
+            // Req#1: Allow the user to input data (names).
             List<string> names = new List<string>();
-            bool exitFlag = false;
-            int listCntr = 0;
-            Console.WriteLine("\nCapacity: {0}", names.Capacity);
+            menuOptns(names);
+            //Console.WriteLine();
+            //REQ#2: Allow the user to display the dataset with the items numbered
 
+        }
+        /*Menu*/
+
+
+        static void menuOptns(List<string> strList)
+        {
+           bool progExitFlg=false;
+           int menuChoice;
+           bool validInt;
             do
             {
-                exitFlag = getUsrInpt(names);
-                listCntr++;
-
-            }while(listCntr <10 && !exitFlag);
-
-            //REQ#2: Allow the user to display the dataset with the items numbered
-            Console.WriteLine("\nCapacity: {0}", names.Count);
-
-            for (int i = 0; i < names.Count; i++)
-            {
-                Console.WriteLine("Record: {0} ", (i + 1) + "  Name: " + names[i]);
-            }
-        }
-
-        /* Get user input*/
-        static bool getUsrInpt(List<string> strList)
-
-        {
-            bool digDetected = false, exitFlag = false;
-            string tempStr = "";
-            // validating if user entered exit sequance
-           
-            do 
-            {
-                digDetected = false;
-                Console.WriteLine("Please Enter a Name:");
-                tempStr = Console.ReadLine(); //store user input in temporary string
-                tempStr = tempStr.Trim();
-                for (int i = 0; i < tempStr.Length; i++)
+                Console.WriteLine("\n\nSelect Menu Option: \n1. Enter Names to Database\n2. Display Names Database\n3. Exit");
+                validInt = int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out menuChoice);
+                if (validInt)
                 {
-                    if (!(Char.IsLetter(tempStr[i]) || Char.IsWhiteSpace(tempStr[i])))
+                    switch (menuChoice)
                     {
-                        digDetected = true;
+                        case 1:
+                            getUsrInpt(strList);
+                            break;
+                        case 2:
+                            if (strList.Count!=0)
+                               { 
+                                Console.WriteLine("\nCapacity: {0}", strList.Count);
+                                for (int i = 0; i < strList.Count; i++)
+                                {
+                                    Console.WriteLine("Record [{0}", (i + 1)+"]" + "   Name:" + strList[i]);
+                                }
+                               }
+                               else
+                               {
+                                    Console.WriteLine("\nEmtpy Records, try menu option 1 to add new records");
+                               }
+                            break;
+                        case 3:
+                            progExitFlg = true;
+                            break;
+                        default:
+                            Console.WriteLine("\nValidation Error: invalid menu choice");
+                            break;
                     }
-                }
-
-                if (digDetected)
-                {
-                    Console.WriteLine("Validation Error: illegal character");
-                }
-                else if (tempStr.ToLower() != "exit")
-                {
-                    strList.Add(tempStr);
                 }
                 else
                 {
-                    exitFlag = true;
+                    Console.WriteLine("Validation Error: unexpected input");
                 }
+            }while(!progExitFlg);
+        }
 
-            }while(digDetected);
-            
-            return exitFlag;
+        /* Get user input*/
+        static void getUsrInpt(List<string> strList)
+
+        {
+            int listCntr = 0;
+            Console.WriteLine("\nCapacity: {0}", strList.Capacity); // for testing
+            bool digDetected = false, exitFlag = false;
+            string tempStr = "";
+            // validating if user entered exit sequance
+            do
+            {
+                //exitFlag = getUsrInpt(names);
+                listCntr++;
+
+                do
+                {
+                    digDetected = false;
+                    Console.Write("Please Enter a Name:");
+                    tempStr = Console.ReadLine(); //store user input in temporary string
+                    tempStr = tempStr.Trim();
+                    for (int i = 0; i < tempStr.Length; i++)
+                    {
+                        if (!(Char.IsLetter(tempStr[i]) || Char.IsWhiteSpace(tempStr[i])))
+                        {
+                            digDetected = true;
+                        }
+                    }
+
+                    if (digDetected)
+                    {
+                        Console.WriteLine("\nValidation Error: invalid character used\n");
+                    }
+                    else if (tempStr.ToLower() != "exit")
+                    {
+                        strList.Add(tempStr);
+                    }
+                    else
+                    {
+                        exitFlag = true;
+                    }
+
+                } while (digDetected);
+
+                //return exitFlag;
+
+            } while (listCntr < 10 && !exitFlag);
 
         }
     }
